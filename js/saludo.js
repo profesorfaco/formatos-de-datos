a2a7e86678d43fd12360625436e4ec345eb7e074
Felipe Cortez <hola@faco.cl>
2019-04-23 10:02:32 -0400

    Update saludo.js

--- js/saludo.js
+++ js/saludo.js
@@ -1,11 +1,31 @@
 var profesor = {
-	nombre: ["Felipe", "Cortez"],
+	nombre: [
+		"Felipe", 
+		"Alberto", 
+		"Cortez", 
+		"Orellana"
+	],
 	apodo: "faco",
 	correo: "profesor@faco.cl",
-	estudios: {pregrado:"Diseño Gráfico", postgrado:"Filosofía", diplomas: ["Métodos Mixtos en Investigaciones Sociales", "Docencia Universitaria","Diseño de Interacción y physical computing","Filosofía y Estética","Filosofía"]}, 
-	intereses: ["Docencia", "Diseño", "Fenomenología", "Bicicleta"],
+	estudios: {
+		pregrado:"Diseño Gráfico", 
+		postgrado:"Filosofía", 
+		diplomas: [
+			"Métodos Mixtos en Investigaciones Sociales", 
+			"Docencia Universitaria",
+			"Diseño de Interacción y physical computing",
+			"Filosofía y Estética",
+			"Filosofía"
+		]
+	}, 
+	intereses: [
+		"Docencia", 
+		"Diseño", 
+		"Fenomenología", 
+		"Bicicleta"
+	],
 	saludo: function(){
-		document.write("Hola, mi nombre es " + this.nombre[0] + " " + this.nombre[1])
+		alert("Hola, mi nombre es " + this.nombre[0] + " " + this.nombre[2])
 	},
 	repositorio: function(){
 		window.location.replace("https://github.com/profesorfaco/datos/")
