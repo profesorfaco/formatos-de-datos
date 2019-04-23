dbdcc83ffe6a3a6b700c85072f805c7a9aa6f8de
Felipe Cortez <hola@faco.cl>
2019-04-23 10:00:13 -0400

    Update saludo.js

--- js/saludo.js
+++ js/saludo.js
@@ -5,7 +5,7 @@ var profesor = {
 	estudios: {pregrado:"Diseño Gráfico", postgrado:"Filosofía", diplomas: ["Métodos Mixtos en Investigaciones Sociales", "Docencia Universitaria","Diseño de Interacción y physical computing","Filosofía y Estética","Filosofía"]}, 
 	intereses: ["Docencia", "Diseño", "Fenomenología", "Bicicleta"],
 	saludo: function(){
-		document.write("Hola, mi nombre es " + this.nombre[0] + " " + this.nombre[2])
+		document.write("Hola, mi nombre es " + this.nombre[0] + " " + this.nombre[1])
 	},
 	repositorio: function(){
 		window.location.replace("https://github.com/profesorfaco/datos/")
